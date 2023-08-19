dcc273ac66d81ddce38dbb79c520735f1518693c
SubstanceShift <54613628+MuhammedKAldin@users.noreply.github.com>
2023-08-19 12:19:19 +0300

    no message

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express');
+const puppeteer = require('puppeteer');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Define a route for your API
-app.get('/api', (req, res) => {
+app.get('/api/steam-market', (req, res) => {
   const data = {
     message: 'Hello, this is your API!'
   };
   res.json(data);
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+async function fetchSteamPrice() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://store.steampowered.com/app/728880/Overcooked_2');
+  
+  // Wait for the price element to load
+  await page.waitForSelector('.game_purchase_price');
+  
+  const price = await page.$eval('.game_purchase_price', element => element.textContent);
+  
+  await browser.close();
+  
+  return price.trim();
+}
+
+fetchSteamPrice().then(price => {
+  console.log('Price:', price);
 });
